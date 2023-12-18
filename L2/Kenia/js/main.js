79db5da886f3e3f548591ce08829877a2b32fe76
Malacatunga <mmontecinom2223@politecnics.barcelona>
2023-12-18 05:47:53 +0100

    último update
    que se acabe ya gracias

--- L2/Kenia/js/main.js
+++ L2/Kenia/js/main.js
@@ -1,27 +1,27 @@
 // Variables del juego
-let game = document.getElementById('game');
-let personaje = document.getElementById('personaje');
+const game = document.getElementById('game');
+const personaje = document.getElementById('personaje');
+const imagenPersonaje = document.getElementById('imagenPersonaje');
 let leftPos = 630;
 let topPos = 600;
 let arrayCamino;
 let intervalId = null;
-let vidas = 3;
 let juegoEnCurso = true;
-const imagenPersonaje = document.getElementById('imagenPersonaje');
-let direction = 'up'; // La dirección del movimiento controlado por teclado
+let direction = 'up'; // dirección default
 
 // Variables de puntuación
+let vidas = 3;
 let puntosVidas = 6;
 let puntosObjetos = 0;
 let puntosTiempo;
-let puntosTiempoMax = 13;
-let puntosXsegundo = 0.5;
+const puntosTiempoMax = 13;
+const puntosXsegundo = 0.5;
 
 // Variables del temporizador
-let gameTimer;
+let gameTimer; // intervalo de tiempo
 let tiempoTranscurrido = 0;
 let colisionConObjeto = false;
-console.log(tiempoTranscurrido);
+
 
 //Puntuación
 document.getElementById("puntuacionVidas").innerHTML = puntosVidas;
@@ -38,15 +38,13 @@ spawnObjeto("/M12/L2/Kenia/images/objetos/piedra.png");
 spawnObjeto("/M12/L2/Kenia/images/objetos/paloBuenoB.png");
 spawnObjeto("/M12/L2/Kenia/images/objetos/placasolarBuenaB.png");
 
-inicioMovimientoAutomatico(); // que se mueva desde el principio
-movimientoPersonaje();
+inicioMovimientoAutomaticoPersonaje(); // que se mueva desde el principio
+cambioDeDireccionPersonaje();
 
 // Funciones de puntuación
-function puntuacionFinal() {
-
-  let sumaPuntos;
-  sumaPuntos = puntosObjetos + puntosTiempo + puntosVidas;
-  document.getElementById('sumaPuntos').innerHTML = sumaPuntos;
+function calcularPuntuacionFinal() {
+  const sumaPuntos = puntosObjetos + puntosTiempo + puntosVidas;
+  document.getElementById('sumaPuntos').innerHTML = sumaPuntos + '/25';
   document.cookie = "puntos=" + sumaPuntos + ";  path=/;";
 
 }
@@ -74,10 +72,8 @@ function actualizarVidas() {
   for (let i = 1; i <= 3; i++) {
     const heart = document.getElementById(`heart${i}`);
     if (i <= vidas) {
-      // Mostrar corazón lleno
       heart.src = "/M12/L2/Kenia/images/corazones/corazonPixel-entero.png";
     } else {
-      // Ocultar corazón vacío
       heart.src = "/M12/L2/Kenia/images/corazones/corazonPixel-vacio.png";
     }
   }
@@ -88,8 +84,7 @@ function actualizarVidas() {
 function ganar() {
   if (leftPos === 0) {
     detenerTemporizador();
-    puntuacionFinal();
-
+    calcularPuntuacionFinal();
     popupFuncion('ganar');
   }
 }
@@ -150,7 +145,7 @@ function crearMapa() {
 
         let caminoDiv = document.createElement('div');
         caminoDiv.className = 'camino';
-        //ajusta las posiciones 
+        //ajustar las posiciones 
         caminoDiv.style.top = i * 80 + 'px';
         caminoDiv.style.left = j * 100 + 'px';
         game.appendChild(caminoDiv);
@@ -161,20 +156,17 @@ function crearMapa() {
 
 // Funciones de personaje
 function actualizarPosicionPersonaje() {
-
   ganar();
-  comprobarColisionObjeto();
+  comprobarColisionObjetoPersonaje();
   //posición personaje
   personaje.style.left = leftPos + 'px';
   personaje.style.top = topPos + 'px';
 
-  //comprobar que no se sale
   comprobarPersonajeDentroCamino();
 
 }
 
-function inicioMovimientoAutomatico() {
-
+function inicioMovimientoAutomaticoPersonaje() {
   if (juegoEnCurso && vidas > 0) {
     intervalId = movimiento('up', 10);
   }
@@ -194,11 +186,11 @@ function movimiento(direction, step) {
     actualizarPosicionPersonaje();
 
 
-  }, 60); //VELOCIDAD  PONER A 80 LUEGO
+  }, 65); //VELOCIDAD  PONER A 80 LUEGO
 }
 
 
-function movimientoPersonaje() {
+function cambioDeDireccionPersonaje() {
   document.addEventListener('keydown', (event) => {
     if (juegoEnCurso) {
 
@@ -251,17 +243,17 @@ function comprobarPersonajeDentroCamino() {
   //1
   const topLeftColumn = Math.floor((leftTop - 10) / 100);
   const topLeftRow = Math.floor((leftBottom - 10) / 80);
-  // //2
+  //2
   const topRightColumn = Math.floor((rightTop) / 100);
   const topRightRow = Math.floor((leftBottom - 10) / 80);
-  // //3
-  const bottomLeftColumn = Math.floor((leftTop - 10) / 100); //lado
-  const bottomLeftRow = Math.floor((rightBottom - 10) / 80); //abajo
-  // //4
+  //3
+  const bottomLeftColumn = Math.floor((leftTop - 10) / 100); 
+  const bottomLeftRow = Math.floor((rightBottom - 10) / 80); 
+  //4
   const bottomRightColumn = Math.floor((rightTop - 10) / 100);
   const bottomRightRow = Math.floor((rightBottom - 10) / 80);
 
-  //blocked si es la posición 0
+  //blocked si la posición en la array es 0
   const topLeftIsBlocked = arrayCamino[topLeftRow][topLeftColumn] === 0;
   const topRightIsBlocked = arrayCamino[topRightRow][topRightColumn] === 0;
   const bottomLeftIsBlocked = arrayCamino[bottomLeftRow][bottomLeftColumn] === 0;
@@ -274,7 +266,7 @@ function comprobarPersonajeDentroCamino() {
     puntosVidas = puntosVidas - 2;
     document.getElementById("puntuacionVidas").innerHTML = puntosVidas;
     actualizarVidas();
-    inicioMovimientoAutomatico();
+    inicioMovimientoAutomaticoPersonaje();
 
     //restablece la posición
     leftPos = 630;
@@ -287,6 +279,14 @@ function comprobarPersonajeDentroCamino() {
 }
 
 // Funciones de objetos
+function generarPosicionAleatoriaObjeto() {
+  const randomX = Math.floor(Math.random() * 8);
+  const randomY = Math.floor(Math.random() * 8);
+  const objetoLeft = randomY * 100;
+  const objetoTop = randomX * 80;
+  return { objetoLeft, objetoTop };
+}
+
 function spawnObjeto(imagenSrc) {
   const imagenObjeto = document.createElement('img');
   imagenObjeto.src = imagenSrc;
@@ -294,9 +294,9 @@ function spawnObjeto(imagenSrc) {
   let posicionValida = false;
 
   while (!posicionValida) {
-    const { objetoLeft, objetoTop } = generarPosicionAleatoria();
+    const { objetoLeft, objetoTop } = generarPosicionAleatoriaObjeto();
 
-    if (!verificarEsquinasEnCamino(objetoLeft, objetoTop, imagenObjeto) && !verificarColisionObjetos(objetoLeft, objetoTop, imagenObjeto)) {
+    if (!comprobarObjetoDentroCamino(objetoLeft, objetoTop, imagenObjeto)) {
       // Si la posición es válida y no hay colisiones con otros objetos, salir del bucle
       posicionValida = true;
 
@@ -315,7 +315,33 @@ function spawnObjeto(imagenSrc) {
   imagenObjeto.haColisionado = false;
 }
 
-function comprobarColisionObjeto() {
+
+
+function comprobarObjetoDentroCamino(objetoLeft, objetoTop, imagenObjeto) {
+  const objetoTopLeftRow = Math.floor(objetoTop / 80);
+  const objetoTopLeftColumn = Math.floor(objetoLeft / 100);
+
+  const objetoTopRightRow = Math.floor(objetoTop / 80);
+  const objetoTopRightColumn = Math.floor((objetoLeft + imagenObjeto.offsetWidth) / 100);
+
+  const objetoBottomLeftRow = Math.floor((objetoTop + imagenObjeto.offsetHeight) / 80);
+  const objetoBottomLeftColumn = Math.floor(objetoLeft / 100);
+
+  const objetoBottomRightRow = Math.floor((objetoTop + imagenObjeto.offsetHeight) / 80);
+  const objetoBottomRightColumn = Math.floor((objetoLeft + imagenObjeto.offsetWidth) / 100);
+
+  const esquinasBloqueadas =
+    arrayCamino[objetoTopLeftRow][objetoTopLeftColumn] === 0 ||
+    arrayCamino[objetoTopRightRow][objetoTopRightColumn] === 0 ||
+    arrayCamino[objetoBottomLeftRow][objetoBottomLeftColumn] === 0 ||
+    arrayCamino[objetoBottomRightRow][objetoBottomRightColumn] === 0;
+
+  return esquinasBloqueadas;
+}
+
+
+
+function comprobarColisionObjetoPersonaje() {
 
   const personajeWidth = personaje.offsetWidth;
   const personajeHeight = personaje.offsetHeight;
@@ -340,7 +366,6 @@ function comprobarColisionObjeto() {
         rightBottom > objetoTop
       ) {
         // Colisión con el objeto
-
         puntosObjetos += 2;
         document.getElementById('puntuacionObjetos').innerHTML = puntosObjetos;
         objeto.remove();
@@ -351,66 +376,7 @@ function comprobarColisionObjeto() {
   });
 }
 
-function verificarColisionObjetos(objetoLeft, objetoTop, imagenObjeto) {
-  const objetos = document.querySelectorAll('.objeto');
-
-  for (const objeto of objetos) {
-    if (!objeto.haColisionado) {
-      const objetoWidth = objeto.offsetWidth;
-      const objetoHeight = objeto.offsetHeight;
-      const existingObjetoLeft = parseInt(objeto.style.left);
-      const existingObjetoTop = parseInt(objeto.style.top);
-
-      if (
-        objetoLeft < existingObjetoLeft + objetoWidth &&
-        objetoLeft + imagenObjeto.offsetWidth > existingObjetoLeft &&
-        objetoTop < existingObjetoTop + objetoHeight &&
-        objetoTop + imagenObjeto.offsetHeight > existingObjetoTop
-      ) {
-        // Hay colisión con otro objeto
-        objeto.remove();
-        objeto.haColisionado = true;
-
-        return true;
-      }
-    }
-  }
-
-  return false;
-}
-
-function generarPosicionAleatoria() {
-  const randomX = Math.floor(Math.random() * 8);
-  const randomY = Math.floor(Math.random() * 8);
-  const objetoLeft = randomY * 100;
-  const objetoTop = randomX * 80;
-  return { objetoLeft, objetoTop };
-}
-
-function verificarEsquinasEnCamino(objetoLeft, objetoTop, imagenObjeto) {
-  const objetoTopLeftRow = Math.floor(objetoTop / 80);
-  const objetoTopLeftColumn = Math.floor(objetoLeft / 100);
-
-  const objetoTopRightRow = Math.floor(objetoTop / 80);
-  const objetoTopRightColumn = Math.floor((objetoLeft + imagenObjeto.offsetWidth) / 100);
-
-  const objetoBottomLeftRow = Math.floor((objetoTop + imagenObjeto.offsetHeight) / 80);
-  const objetoBottomLeftColumn = Math.floor(objetoLeft / 100);
-
-  const objetoBottomRightRow = Math.floor((objetoTop + imagenObjeto.offsetHeight) / 80);
-  const objetoBottomRightColumn = Math.floor((objetoLeft + imagenObjeto.offsetWidth) / 100);
-
-  const esquinasBloqueadas =
-    arrayCamino[objetoTopLeftRow][objetoTopLeftColumn] === 0 ||
-    arrayCamino[objetoTopRightRow][objetoTopRightColumn] === 0 ||
-    arrayCamino[objetoBottomLeftRow][objetoBottomLeftColumn] === 0 ||
-    arrayCamino[objetoBottomRightRow][objetoBottomRightColumn] === 0;
-
-  return esquinasBloqueadas;
-}
-
 //Funciones de redirección
-
 function volverAlMapa() {
   window.location.href = "../../controllers_php/updatePuntosController.php";
 }
